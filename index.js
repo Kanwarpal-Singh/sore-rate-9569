70472d198337f6a8b5ff9837cff118b136c8c069
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2023-03-29 23:47:04 +0530

    day-2

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ require("dotenv").config()
 const {userRouter} = require("./routes/userroute")
 const {connection} = require("./db")
 const cors = require("cors")
+const { urlencoded } = require("express")
+const qrcode = require("qrcode")
+const {qrRouter} = require("./routes/qrRouter")
 
 app.use(cors())
 app.use(express.json())
+app.use(urlencoded({extended:false}))
 
 
 app.use("/user",userRouter)
+app.use("/qr",qrRouter)
 
 
 app.listen(process.env.port,async()=>{
