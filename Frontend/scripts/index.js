45d238e3df422a1d75ad01e7b9954ddd136978d5
Kanwarpal-Singh <111496199+Kanwarpal-Singh@users.noreply.github.com>
2023-04-01 14:13:41 +0530

    login page

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -2,3 +2,7 @@ let signupbtn = document.getElementById("signupbtn")
 signupbtn.addEventListener("click",function(){
  window.location.href= "./signup.html"
 })
+let loginbtn = document.getElementById("loginbtn")
+loginbtn.addEventListener("click",function(){
+ window.location.href= "./login.html"
+})
